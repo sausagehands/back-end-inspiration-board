00ed82acf28f48918ec282aa0a4f96a7feba66c0
Madi Rei <madison.nicole.rei@gmail.com>
2026-01-05 14:33:08 -0800

    adds board routes

--- app/routes/board_route.py
+++ app/routes/board_route.py
@@ -5,3 +5,43 @@ from app.models.board import Board
 from ..db import db
 
 bp = Blueprint("board_bp",__name__, url_prefix="/boards")
+
+# CREATE
+@bp.post("")
+def create_board():
+    request_body = request.get_json()
+    return create_model(Board, request_body)
+
+# READ
+@bp.get("")
+def get_all_boards():
+    return get_models_with_filters(Board, request.args)
+
+@bp.get("/<board_id>")
+def get_board_by_id(board_id):
+    board = validate_model(Board, board_id)
+    return board.to_dict()
+
+# UPDATE
+@bp.put("/<board_id>")
+def update_board(board_id):
+    board = validate_model(Board, board_id)
+    request_body = request.get_json()
+
+    if "title" in request_body:
+        board.title = request_body["title"]
+    if "owner" in request_body:
+        board.owner = request_body["owner"]
+        
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+# DELETE
+@bp.delete("/<board_id>")
+def delete_board(board_id):
+    board = validate_model(Board, board_id)
+    db.session.delete(board)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
